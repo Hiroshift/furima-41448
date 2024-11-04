57307decf036a17af8bab2d636a20cf3306c6883
Hiroshift <ooh-baby@buggies.co.jp>
2024-11-04 23:58:01 +0900

    ログイン・ログアウト機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # ニックネームが必須
+  validates :nickname, presence: true
+
+  # 名前が必須で全角（漢字・ひらがな・カタカナ）であること
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'は全角文字を使用してください' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'は全角文字を使用してください' }
+
+  # 名前のカナが必須で全角カタカナであること
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナを使用してください' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナを使用してください' }
+
+  # 生年月日が必須
+  validates :birth_date, presence: true
 end
