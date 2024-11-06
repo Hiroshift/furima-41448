b59d39d74abb06079f4c545e2ac76c434539a113
Hiroshift <ooh-baby@buggies.co.jp>
2024-11-06 22:29:09 +0900

    ユーザーのバリデーションと単体テストを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,17 @@ class User < ApplicationRecord
 
   # ニックネームが必須
   validates :nickname, presence: true
+  
+  # パスワードが英数字混合であること
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'is invalid. Include both letters and numbers' }
 
   # 名前が必須で全角（漢字・ひらがな・カタカナ）であること
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'は全角文字を使用してください' }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'は全角文字を使用してください' }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'is invalid. Input full-width characters' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'is invalid. Input full-width characters' }
 
   # 名前のカナが必須で全角カタカナであること
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナを使用してください' }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナを使用してください' }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters' }
 
   # 生年月日が必須
   validates :birth_date, presence: true
