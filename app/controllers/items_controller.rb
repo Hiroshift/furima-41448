ed6a09df564b3145e2bd9b09def0ed1f43ed02e9
Hiroshift <ooh-baby@buggies.co.jp>
2024-11-09 19:23:16 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,3 +3,9 @@ class ItemsController < ApplicationController
     # 処理
   end
 end
+
+private
+
+def item_params
+  params.require(:item).permit(:item_name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
+end
