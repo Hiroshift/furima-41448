9a59b647f8fa934adb9a2ac88b3bc181a9cd8ae3
Hiroshift <ooh-baby@buggies.co.jp>
2024-11-12 23:55:07 +0900

    価格入力に応じた手数料・利益表示機能（javascript)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,10 +8,19 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
 private
 
-def item_params
-  params.require(:item).permit(:item_name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
+  def item_params
+    params.require(:item).permit(:item_name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -8,17 +8,14 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_day
 
-  validates :category_id, numericality: { other_than: 1, message: "Category can't be blank" }
-  validates :condition_id, numericality: { other_than: 1, message: "Sales status can't be blank" }
-  validates :shipping_fee_id, numericality: { other_than: 1, message: "Shipping fee status can't be blank" }
-  validates :prefecture_id, numericality: { other_than: 1, message: "Prefecture can't be blank" }
-  validates :shipping_day_id, numericality: { other_than: 1, message: "Scheduled delivery can't be blank" }
-
-  validates :image, presence: { message: "Image can't be blank" }
-  validates :item_name, presence: { message: "Name can't be blank" }
-  validates :description, presence: { message: "Info can't be blank" }
-
-  validates :price, presence: { message: "Price can't be blank" }, 
-                  numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "Price is out of setting range" }
-
+  validates :image, presence: true 
+  validates :item_name, presence: true 
+  validates :description, presence: true 
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_fee_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
 end
