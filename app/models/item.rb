43f26894df3a33ce4245f4e8d21088ea03965611
Hiroshift <ooh-baby@buggies.co.jp>
2024-11-13 20:52:41 +0900

    テストコードの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,8 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_day
 
+  belongs_to :user
+
   validates :image, presence: true 
   validates :item_name, presence: true 
   validates :description, presence: true 
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name { Faker::Commerce.product_name }
+    description { Faker::Lorem.paragraph }
+    price { Faker::Number.between(from: 300, to: 9_999_999) }
+
+    association :user
+
+    category_id { 2 } 
+    condition_id { 2 }
+    shipping_fee_id { 2 }
+    prefecture_id { 2 }
+    shipping_day_id { 2 }
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
