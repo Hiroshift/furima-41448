589331c4fac544b48b15c61191b035d68e178dcd
Hiroshift <ooh-baby@buggies.co.jp>
2024-11-24 14:21:47 +0900

    edit.html.erbを編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new,:edit]
+  before_action :authenticate_user!, only: [:new,:show,:edit]
 
   def index
     @items = Item.order('created_at DESC')
